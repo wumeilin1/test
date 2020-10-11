9e7f35d3c10a531171b149627711cbaacf333c02
wumeilin <72651111+wumeilin1@users.noreply.github.com>
2020-10-11 10:31:10 +0800

    Add files via upload

---
+++ Calcultor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test2_8
+{
+    class Calcultor
+    {
+        public string a;
+        public string b;
+        public string c;
+        public int e;
+        public int f;
+        public void setmessage()
+        {
+            Console.WriteLine("请输入第一个数字或字符串：");
+            a = Console.ReadLine();
+            Console.WriteLine("请输入运算符（+ - * /） ：");
+            c = Console.ReadLine();
+            Console.WriteLine("请输入第二个数字或字符串：");
+            b = Console.ReadLine();
+        }
+        public void getmessage()
+        {
+            int d = 0;
+            if (int.TryParse(a, out d) && int.TryParse(b, out d))
+            {
+               e = int.Parse(a);
+               f = int.Parse(b);
+               jisuan();
+            }
+            else
+                Connection();
+        }
+        public void Connection()
+        {
+            string d;
+            if (c == "+")
+            {
+                d = a + b;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+            else if (c == "-")
+            {
+                d = a.Replace(b, "");
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+        }
+        public void jisuan()
+        {
+            float d;
+            if (c == "+")
+            {
+                d = e + f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+            else
+                if (c == "-")
+                {
+                    d = e - f;
+                    Console.WriteLine(a + c + b + '=' + d);
+                }
+             else
+                if (c == "*")
+                 {
+                     d = e * f;
+                     Console.WriteLine(a + c + b + '=' + d);
+                  }
+             else
+                 if (c == "/")
+                  {
+                      d = e / f;
+                      Console.WriteLine(a + c + b + '=' + d);
+                  }
+             else
+                  Console.WriteLine("符号输入错误");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test2_8
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcultor a = new Calcultor();
+            a.setmessage();
+            a.getmessage();
+        }
+    }
+}
